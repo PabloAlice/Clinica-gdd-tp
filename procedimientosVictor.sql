a514c1e49cf7cb38354a0ef72a8a90484ace14aa
vmontanez <victor_vereau06@hotmail.com>
2016-11-07 13:03:29 -0300

    Actualizo excel y ProcedimientoVictor con topEspecialidadesMasCancelaciones

--- procedimientosVictor.sql
+++ procedimientosVictor.sql
@@ -36,3 +36,23 @@ if ((select estado from FORANEOS.usuario where id=@idAfiliado) = 1 )
 	 where id_afiliado=@nroAfiliado
 	   and fecha=@fecha_compra
 end
+
+
+--topEspecialidadesMasCancelaciones
+IF OBJECT_ID('FORANEOS.topEspecialidadesMasCancelaciones') IS NOT NULL
+	DROP PROCEDURE FORANEOS.topEspecialidadesMasCancelaciones;
+GO
+ ALTER procedure [FORANEOS].[topEspecialidadesMasCancelaciones](@anio numeric, @semestre numeric)
+ as
+ begin
+   select top 5 e.descripcion , count(1) cantidad 
+	 from FORANEOS.cancelacion_turno ct,FORANEOS.turno t, 
+	      FORANEOS.horario_atencion ha, FORANEOS.especialidad e
+	where ct.numero=t.numero
+	  and t.id_horario_atencion=ha.id
+	  and ha.codigo_especialidad= e.codigo
+	  and year(ha.fecha) = @anio
+	  and CEILING(MONTH(ha.fecha)/6.00)=@semestre
+	group by e.descripcion
+	order by 2 desc
+ end
