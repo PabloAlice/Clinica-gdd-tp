be8712f3b5940c3f80a88107223353e90e359906
PabloAlice <pablo_exe@hotmaill.com>
2016-11-14 01:05:10 -0300

    Arreglo pedido de turno para registrar fecha llegada

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -1398,7 +1398,7 @@ begin
 
 	SELECT t.numero, u.id, a.numero_afiliado, u.nombre,ha.fecha, u.apellido
     from FORANEOS.Profesional p inner join FORANEOS.Horario_Atencion ha on p.id = ha.id_agenda inner join FORANEOS.Turno t on t.id_horario_atencion = ha.id inner join FORANEOS.Afiliado a on
-	a.id = t.id_afiliado inner join FORANEOS.Usuario u on a.id = u.id where p.id = @id_profesional and convert(date,ha.fecha) = @fecha
+	a.id = t.id_afiliado inner join FORANEOS.Usuario u on a.id = u.id where p.id = @id_profesional and convert(date,ha.fecha) = @fecha AND t.fecha_llegada is null
 	
 		
 end
